1caa0eb316dfaa4049cfb22fd9bfd3d75d921e5d
afethi <fethiabderrahmane1@gmail.com>
2025-10-03 17:36:30 +0100

    window creation and points draw

---
+++ Jaiking.java
@@ -0,0 +1,71 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class Jaiking extends JFrame {
+
+    private ArrayList<Point> points = new ArrayList<>();
+
+    public Jaiking() {
+        setTitle("Jaiking");
+        setSize(900, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        DrawingPanel panel = new DrawingPanel();
+        panel.setFocusable(true);
+        panel.requestFocusInWindow();
+        add(panel);
+
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                points.add(e.getPoint());
+                panel.repaint();
+            }
+        });
+
+        panel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyChar() == 'c' || e.getKeyChar() == 'C') {
+                    points.clear();
+                    panel.repaint();
+                }
+
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    System.exit(0);
+                }
+
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    System.out.println("Space Clicked");
+                    // Start the chaikin animation
+                }
+            }
+        });
+    }
+
+    private class DrawingPanel extends JPanel {
+
+        public DrawingPanel() {
+            setBackground(Color.BLACK);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.GRAY);
+            for (Point p : points) {
+                g.drawOval(p.x - 3, p.y - 3, 6, 6);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Jaiking drawer = new Jaiking();
+            drawer.setVisible(true);
+        });
+    }
+}
